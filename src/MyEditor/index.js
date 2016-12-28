deedad2a69046e4c54146770478097e647a6e087
Yanbin Ma <ma.yanbin@qq.com>
2016-12-28 10:54:52 +0800

    添加 Media 资源的功能

--- src/MyEditor/index.js
+++ src/MyEditor/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Editor, EditorState, RichUtils, getDefaultKeyBinding, KeyBindingUtil, convertToRaw } from 'draft-js';
+import { Editor, EditorState, Entity, CompositeDecorator, RichUtils, AtomicBlockUtils, getDefaultKeyBinding, KeyBindingUtil, convertToRaw } from 'draft-js';
 
 import './index.css';
 
@@ -11,12 +11,97 @@ const styleMap = {
     fontSize: 16,
     padding: 2,
   },
+  UNDERLINE: {
+    textDecoration: 'none',
+    borderBottom: '1px solid',
+  }
 };
+
+
+const getBlockStyle = (block) => {
+  switch (block.getType()) {
+    case 'blockquote': return 'RichEditor-blockquote';
+    default: return null;
+  }
+}
+
+const findLinkEntities = (contentBlock, callback) => {
+  contentBlock.findEntityRanges(
+    (character) => {
+      const entityKey = character.getEntity();
+      return (
+        entityKey !== null &&
+        Entity.get(entityKey).getType() === 'LINK'
+      );
+    },
+    callback
+  );
+}
+
+const Link = (props) => {
+  const {url} = Entity.get(props.entityKey).getData();
+  return (
+    <a href={url} style={{color: '#3b5998', textDecoration: 'none', borderBottom: '1px solid'}}>
+      {props.children}
+    </a>
+  );
+};
+
+
+const Image = (props) => {
+  return (
+    <div>
+      <img src={props.src} alt={props.description} />
+      <figcaption>{props.description}</figcaption>
+    </div>
+  )
+}
+
+const Video = (props) => {
+  return (
+    <div>
+      <video controls src={props.src} style={{maxWidth: '100%'}} />
+      <figcaption>{props.description}</figcaption>
+    </div>
+  )
+}
+
+const Media = (props) => {
+  const entity = Entity.get(props.block.getEntityAt(0));
+  const type = entity.getType();
+  const {src, description} = entity.getData();
+
+  switch(type) {
+    case 'IMAGE': return <Image src={src} description={description} />;
+    case 'VIDEO': return <Video src={src} description={description} />;
+    default: return null;
+  }
+}
+
+
+const myMediaBlockRenderer = (block) => {
+  const type = block.getType();
+  if (type === 'atomic') {
+    return {
+      component: Media,
+      editable: false
+    };
+  }
+
+  return null;
+}
 
 class MyEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+    const decorator = new CompositeDecorator([
+      {
+        strategy: findLinkEntities,
+        component: Link,
+      },
+    ]);
+
+    this.state = {editorState: EditorState.createEmpty(decorator)};
 
     this.focus = () => this.refs.editor.focus();
     this.onChange = (editorState) => this.setState({editorState});
@@ -25,6 +110,10 @@ class MyEditor extends React.Component {
     this.onTab = (e) => this._onTab(e);
     this.toggleBlockType = (type) => this._toggleBlockType(type);
     this.toggleInlineStyle = (style) => this._toggleInlineStyle(style);
+
+    this.insertLink = () => this._insertLink();
+    this.insertImage = () => this._insertImage();
+    this.insertVideo = () => this._insertVideo();
   }
 
   myKeyBindingFn(e) {
@@ -53,6 +142,41 @@ class MyEditor extends React.Component {
     this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
   }
 
+  _insertLink() {
+    const {editorState} = this.state;
+    const entityKey = Entity.create('LINK', 'MUTABLE', {url: 'https://myanbin.github.io'});
+    this.onChange(
+      RichUtils.toggleLink(
+        editorState,
+        editorState.getSelection(),
+        entityKey
+      )
+    );
+  }
+
+  _insertImage() {
+    const {editorState} = this.state;
+    const entityKey = Entity.create('IMAGE', 'IMMUTABLE', {src: 'http://placekitten.com/g/200/200', description: 'Pretty Cat'});
+    this.onChange(
+      AtomicBlockUtils.insertAtomicBlock(
+        editorState,
+        entityKey,
+        ' '
+      )
+    )
+  }
+  _insertVideo() {
+    const {editorState} = this.state;
+    const entityKey = Entity.create('VIDEO', 'IMMUTABLE', {src: 'http://images.apple.com/media/cn/macbook-pro/2016/b4a9efaa_6fe5_4075_a9d0_8e4592d6146c/films/design/macbook-pro-design-tft-cn-20161026_1536x640h.mp4', description: 'MacBook Pro'});
+    this.onChange(
+      AtomicBlockUtils.insertAtomicBlock(
+        editorState,
+        entityKey,
+        ' '
+      )
+    )
+  }
+
   _toggleBlockType(blockType) {
     this.onChange(
       RichUtils.toggleBlockType(
@@ -63,6 +187,7 @@ class MyEditor extends React.Component {
   }
 
   _toggleInlineStyle(inlineStyle) {
+    console.log('change inline style: ', inlineStyle);
     this.onChange(
       RichUtils.toggleInlineStyle(
         this.state.editorState,
@@ -94,10 +219,16 @@ class MyEditor extends React.Component {
           editorState={editorState}
           onToggle={this.toggleInlineStyle}
         />
+        <div className="RichEditor-controls">
+          <StyleButton label="Link" onToggle={this.insertLink} />
+          <StyleButton label="Image" onToggle={this.insertImage} />
+          <StyleButton label="Video" onToggle={this.insertVideo} />
+        </div>
         <div className={className} onClick={this.focus}>
           <Editor
             blockStyleFn={getBlockStyle}
             customStyleMap={styleMap}
+            blockRendererFn={myMediaBlockRenderer}
             editorState={editorState}
             handleKeyCommand={this.handleKeyCommand}
             keyBindingFn={this.myKeyBindingFn}
@@ -115,12 +246,6 @@ class MyEditor extends React.Component {
 
 
 
-function getBlockStyle(block) {
-  switch (block.getType()) {
-    case 'blockquote': return 'RichEditor-blockquote';
-    default: return null;
-  }
-}
 
 class StyleButton extends React.Component {
   constructor() {
@@ -144,6 +269,7 @@ class StyleButton extends React.Component {
     );
   }
 }
+
 
 const BLOCK_TYPES = [
   {label: 'H1', style: 'header-one'},
@@ -181,7 +307,7 @@ const BlockStyleControls = (props) => {
   );
 };
 
-var INLINE_STYLES = [
+const INLINE_STYLES = [
   {label: 'Bold', style: 'BOLD'},
   {label: 'Italic', style: 'ITALIC'},
   {label: 'Underline', style: 'UNDERLINE'},
@@ -204,5 +330,7 @@ const InlineStyleControls = (props) => {
     </div>
   );
 };
+
+
 
 export default MyEditor
