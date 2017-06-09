949fe11d08ce181ca2fb1fe863ee6dc8400fcad1
Yanbin Ma <yianbin@qq.com>
2017-06-09 17:05:22 +0800

    增强编辑器统计功能

--- src/utils/counter.js
+++ src/utils/counter.js
@@ -1,7 +1,36 @@
+import { convertToRaw } from 'draft-js';
 
 const counter = (editorState) => {
-    const plainText = editorState.getCurrentContent().getPlainText('');
-    return plainText.replace(/\s+/, '').length;
+  const rawContent = convertToRaw(editorState.getCurrentContent());
+  
+  let number_of_words       = 0;
+  let number_of_paragraphs  = 0;
+  let number_of_images      = 0;
+  let number_of_videos      = 0;
+  let number_of_links       = 0;
+
+  rawContent.blocks.forEach(item => {
+    number_of_paragraphs++;
+    number_of_words = number_of_words + item.text.length;
+  });
+
+  Object.keys(rawContent.entityMap).forEach(item => {
+    switch (rawContent.entityMap[item].type) {
+      case 'image':
+        number_of_images++;
+        break;
+      case 'video':
+        number_of_videos++;
+        break;
+      case 'link':
+        number_of_links++;
+        break;
+      default:
+        break;
+    }
+  });
+
+  return { number_of_words, number_of_paragraphs, number_of_images, number_of_videos, number_of_links };
 }
 
 export { counter }
